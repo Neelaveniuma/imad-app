ae9abc8a3392c39c52a8df43cdf33ece57715808
Neelaveniuma <36486195+Neelaveniuma@users.noreply.github.com>
2018-02-20 16:11:57 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,17 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = "New Value";
+
+//move img
+
+var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft += 10;
+    img.style.marginLeft = marginLeft+ 'px';
+}
+
+img.onclick = function(){
+    var interval = setInterval(moveRight, 50);
+};
+
