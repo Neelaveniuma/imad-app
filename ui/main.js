49c5738f87be772051008afd21edee83dd375bd2
Neelaveniuma <36486195+Neelaveniuma@users.noreply.github.com>
2018-02-20 17:26:31 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ button.onclick = function(){
             }
         }
     };
-    request.open('GET', 'http:http://neelsvicky.imad.hasura-app.io/counter', true);
+    request.open('GET', 'http://neelsvicky.imad.hasura-app.io/counter', true);
     request.send(null);
 };
 
