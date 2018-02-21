47e8efaa1e243968f3c973d2985c2cc1d1ab1a27
Neelaveniuma <36486195+Neelaveniuma@users.noreply.github.com>
2018-02-20 17:21:55 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,71 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+
+button.onclick = function(){
+    var request = XMLHttpRequest();
+    request.onreadystatechange= function(){
+        if(request.readystate ===XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET', 'http:http://neelsvicky.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 
@@ -15,5 +82,5 @@ function moveRight(){
 
 img.onclick = function(){
     var interval = setInterval(moveRight, 50);
-};
+};*/
 
