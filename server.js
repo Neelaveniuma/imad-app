f4bee2a3fddbb91cf522864bf2ac432fc270dd9f
Neelaveniuma <36486195+Neelaveniuma@users.noreply.github.com>
2018-02-26 11:37:32 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req,res){
+app.get('articles/:articleName', function (req,res){
     var articleName = req.params.articleName;
    res.send(createTemplate (articles[articleName]));
 });
