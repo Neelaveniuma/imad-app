4eca7a800977cc7ad57fff1da6142328e36459c5
Neelaveniuma <36486195+Neelaveniuma@users.noreply.github.com>
2018-03-01 10:20:55 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -176,13 +176,6 @@ app.get('/counter', function(req, res){
     res.send(counter.toString());
 });
 
-var names = [];
-app.get('/submit-name', function (req, res){
-    var name= req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
-
 var comments = [];
 app.get('/submit-comment', function (req, res){
     var comment= req.query.comment;
@@ -190,8 +183,14 @@ app.get('/submit-comment', function (req, res){
     res.send(JSON.stringify(comments));
 });
 
+var names = [];
+app.get('/submit-name', function (req, res){
+    var name= req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
 
-app.get('articles/:articleName', function (req,res){
+app.get('/:articleName', function (req,res){
     var articleName = req.params.articleName;
    res.send(createTemplate (articles[articleName]));
 });
