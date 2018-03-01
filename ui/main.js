c98888b6432768c85545e7b1c6c851b46edf5cb7
Neelaveniuma <36486195+Neelaveniuma@users.noreply.github.com>
2018-02-28 23:21:18 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -48,11 +48,30 @@ submit.onclick = function(){
 };
 
 
-var commentInput = document.getElementById('comments');
-var comment = commentsInput.value;
+
 var submit = document.getElementById('submit-btn');
 submit.onclick = function(){
+    var request = new XMLHttpRequest();
     
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var comments = request.responseText;
+                comments = JSON.parse(comments);
+                var list = '';
+                for(var i = 0; i<comments.length; i++){
+                    list += '<li>'+ comments + '</li>';
+                }
+                 var ul = document.getElementById('commentlist');
+                 ul.innerHTML = list;
+            }
+            
+        }
+    };
+    var commentInput = document.getElementById('comments');
+    var comment = commentInput.value;
+    request.open('GET', 'http://neelsvicky.imad.hasura-app.io/submit-comments?comment=' + comment , true);
+     request.send(null);
 }
 
 
