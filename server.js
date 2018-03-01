7092ccdeb44e1240e8ca216bbe843a3c723f9391
Neelaveniuma <36486195+Neelaveniuma@users.noreply.github.com>
2018-03-01 10:16:02 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -183,16 +183,16 @@ app.get('/submit-name', function (req, res){
     res.send(JSON.stringify(names));
 });
 
+app.get('/:articleName', function (req,res){
+    var articleName = req.params.articleName;
+   res.send(createTemplate (articles[articleName]));
+});
+
 var comments = [];
 app.get('/submit-comment', function (req, res){
     var comment= req.query.comment;
     comments.push(comment);
     res.send(JSON.stringify(comments));
-});
-
-app.get('/:articleName', function (req,res){
-    var articleName = req.params.articleName;
-   res.send(createTemplate (articles[articleName]));
 });
 
 
