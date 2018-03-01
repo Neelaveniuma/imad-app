1f4f739e284fca85f8ca880d83bf9de57eb372fa
Neelaveniuma <36486195+Neelaveniuma@users.noreply.github.com>
2018-02-28 21:06:56 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,13 +22,15 @@ button.onclick = function(){
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
+    
     var request = new XMLHttpRequest();
+    
     request.onreadystatechange= function(){
         if(request.readyState === XMLHttpRequest.DONE){
              if(request.status === 200){
                     var names = request.responseText;
                     names = JSON.parse(names);
-                     var list = '';
+                    var list = '';
                      for(var i =0; i<names.length; i++){
                          list += '<li>'+ names[i] +'</li>';
                      }
