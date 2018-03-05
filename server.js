04e8805e6f97570d1e887cde358759ee33610543
Neelaveniuma <36486195+Neelaveniuma@users.noreply.github.com>
2018-03-05 13:44:47 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -146,7 +146,7 @@ var articles = {
         },
 };
 
-var pool = new Pool(config);
+
 
 function createTemplate(data){
     var title = data.title;
@@ -196,8 +196,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
 app.get('/test-db',function(req,res){
-	pool.query('select * from test',function(err,result){ 
+	pool.query('SELECT * FROM test',function(err,result){ 
 		if(err){
 			res.status(500).send(err.toString());
 		}
