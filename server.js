60577a2742ae4943af56dfed151238ee72fe0d3f
Neelaveniuma <36486195+Neelaveniuma@users.noreply.github.com>
2018-03-05 14:20:40 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -201,9 +201,9 @@ app.get('/submit-comment', function (req, res){
     res.send(JSON.stringify(comments));
 });
 
-//var pool = new Pool(config);
+var pool = new Pool(config);
 //console.log("config : "+JSON.stringify(config));
-var pool = new pg.Pool(config);
+//var pool = new pg.Pool(config);
 app.get('/test-db', function(req,res){
 	pool.query('SELECT * FROM test', function(err,result){ 
 		if(err){
