3c27cb4708e7c5a4c365b624742a185544ebc2d6
Neelaveniuma <36486195+Neelaveniuma@users.noreply.github.com>
2018-03-05 11:26:28 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -47,11 +47,11 @@ submit.onclick = function(){
     
 };
 
-function getwords() {
+/*function getwords() {
   text = words.value;
   document.getElementById("para").innerHTML += '<p>'+text
   document.getElementById("words").value = "enter"
-}
+}*/
 
 
 
