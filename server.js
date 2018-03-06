8c5c2968db273510db9284ec711070feea4bfe54
Neelaveniuma <36486195+Neelaveniuma@users.noreply.github.com>
2018-03-05 17:58:33 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -169,7 +169,7 @@ var htmlTemplate = `
         <div>
             <h3>${heading}</h3>
         </div>
-        <div>${date}</div>
+        <div>${date.toDateString()}</div>
         <div class="textbox">${content}</div> 
         <div class ="commentbox">${comment}</div>
     </body>
@@ -215,9 +215,19 @@ app.get('/submit-comment', function (req, res){
 
 
 
-app.get('/:articleName', function (req,res){
-    var articleName = req.params.articleName;
-   res.send(createTemplate (articles[articleName]));
+app.get('/articles/:articleName', function (req,res){
+    pool.query("SELECT * FORM article WHERE title = '" + req.params.articleName + "'", function(err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        }else{
+            if(result.rows.length === 0) {
+                res.status(400).send('Article not found');
+            }else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
 });
 
 
