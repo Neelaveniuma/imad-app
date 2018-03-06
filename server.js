9b6bccaa7d05b5f877c918b81576a5ce7c1bf62a
Neelaveniuma <36486195+Neelaveniuma@users.noreply.github.com>
2018-03-05 17:17:51 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -188,7 +188,7 @@ app.get('/test-db', function(req, res) {
       if(err) {
           res.status(500).send(err.toStringigy());
       } else {
-          res.send(JSON.stringify(result));
+          res.send(JSON.stringify(result.rows));
       }
    }); 
 });
