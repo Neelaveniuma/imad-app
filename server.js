53ab4e2d9c3dbbf84f5528c91b2649a825a6812b
Neelaveniuma <36486195+Neelaveniuma@users.noreply.github.com>
2018-03-06 20:43:00 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var config = {
   host: 'db.imad.hasura-app.io',
   user: 'neelsvicky',
@@ -16,6 +17,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret : 'someRandomSecretValue',
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 30}
+}));
 
 var articles = {
         'article-daffodils': {
@@ -223,6 +228,7 @@ app.post('/login', function(req, res){
                  var salt = dbString.split('$')[2];
                  var hashedPassword = hash(password, salt);
                  if(hashedPassword === dbString){
+                     req.session.auth = {userId : result.rows[0].id};
                      res.send('Credentials correct');
                  } else {
                      res.send(403).send('Invalid credentails');
@@ -230,6 +236,19 @@ app.post('/login', function(req, res){
                }
          }
    });  
+});
+
+app.get('/check-login', function(req, res){
+   if(req.session && req.session.auth && req.session.auth.userId){
+       res.send('You are logged in' + req.session.auth.userId.toString());
+   } else {
+       res.send('You are not logged in');
+   }
+});
+
+app.get('/logout', function(req, res){
+   delete req.session.auth;
+   res.send('Logged Out');
 });
 
 var pool = new Pool(config);
